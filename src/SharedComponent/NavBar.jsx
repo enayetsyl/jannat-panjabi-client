508e1933ebd3f81b36b5fb378fc7ff78b77325c3
Enayet <enayetflweb@gamil.com>
2023-11-14 20:58:34 +0600

    footer section update

--- src/SharedComponent/NavBar.jsx
+++ src/SharedComponent/NavBar.jsx
@@ -23,10 +23,10 @@ const NavBar = () => {
       <div className="navbar bg-black text-white opacity-70 fixed z-40">
   <div className="navbar-start">
     <div className="dropdown">
-      <label tabIndex={0} className="btn btn-primary lg:hidden">
+      <label tabIndex={0} className=" text-[#d99904]  lg:hidden mr-10">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-md dropdown-content mt-3 z-[1] p-5 shadow bg-[#d99904] rounded-box w-52">
         {
           navItems
         }
@@ -42,7 +42,7 @@ const NavBar = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="bt">Button</a>
   </div>
 </div>
     </div>
